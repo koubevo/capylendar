a7e33984837d34ce04b38c4444d0047169dacebc
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-17 17:43:06 +0100

    CAPY-4 remove request param

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request): Response
+    public function __invoke(): Response
     {
         return Inertia::render('Dashboard', []);
     }

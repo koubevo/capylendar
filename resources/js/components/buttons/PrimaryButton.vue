0bd63a2a5afb91b77ab10d6fbaa6b2b565dd20f2
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-17 23:21:12 +0100

    CAPY-6 create primary and secondary buttons

---
+++ resources/js/components/buttons/PrimaryButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <UButton color="primary" v-bind="$attrs">
+        <slot />
+    </UButton>
+</template>
---
+++ resources/js/components/buttons/SecondaryButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <UButton color="secondary" variant="subtle" v-bind="$attrs">
+        <slot />
+    </UButton>
+</template>

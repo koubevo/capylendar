a89e5c3367afb0028a783b7bfe6f020cc00b2b2c
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-17 17:35:36 +0100

    CAPY-4 add event and dashboard controller, add store and update event request

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreEventRequest $request)
+    {
+        //
+    }
+
+    public function edit(Event $event)
+    {
+        //
+    }
+
+    public function update(UpdateEventRequest $request, Event $event)
+    {
+        //
+    }
+
+    public function destroy(Event $event)
+    {
+        //
+    }
+}

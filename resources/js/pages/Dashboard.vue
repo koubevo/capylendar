dfd866d07c1c70f3b924f89d1b094ad648756fdd
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-18 21:00:29 +0100

    CAPY-6 add tabs to dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/layouts/app/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const items = [
+    {
+        label: 'Nadcházející',
+        icon: 'i-lucide-rocket',
+        slot: 'coming',
+    },
+    {
+        label: 'Historické',
+        icon: 'i-lucide-history',
+        slot: 'history',
+    },
+];
 </script>
 
 <template>
     <Head title="Domů" />
 
-    <AuthenticatedLayout> </AuthenticatedLayout>
+    <AuthenticatedLayout>
+        <UTabs :items="items">
+            <template #coming>
+                <section class="mt-4">
+                    <h1>Nadcházející</h1>
+                </section>
+            </template>
+
+            <template #history>
+                <section class="mt-4">
+                    <h1>Historické</h1>
+                </section>
+            </template>
+        </UTabs>
+    </AuthenticatedLayout>
 </template>

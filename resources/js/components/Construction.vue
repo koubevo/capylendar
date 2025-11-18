74e24a3085a2a4c4f76317c7abf47cf88dbb57aa
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-18 22:08:56 +0100

    CAPY-6 add construction component

---
+++ resources/js/components/Construction.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex w-full flex-col items-center justify-center">
+        <img
+            src="/images/capys/construction.jpg"
+            alt="Construction"
+            class="w-full md:w-100"
+        />
+        <span class="mt-4 text-center text-xl font-bold"> Makáme na tom! </span>
+    </div>
+</template>
+<script setup lang="ts"></script>

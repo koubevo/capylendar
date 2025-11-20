511c8be212e89d20c3a24d1a0d2ddea3edb29944
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-11-20 22:42:24 +0100

    CAPY-9 add vite config for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,60 @@
 import ui from '@nuxt/ui/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'; // 1. Přidat loadEnv
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+            ui({
+                inertia: true,
+                ui: {
+                    colors: {
+                        primary: 'pink',
+                        secondary: 'sky',
+                        neutral: 'slate',
+                    },
+                    button: {
+                        base: 'cursor-pointer',
+                    },
+                    switch: {
+                        defaultVariants: {
+                            size: 'xl',
+                        },
+                    },
+                    input: {
+                        defaultVariants: {
+                            size: 'xl',
+                        },
+                    },
+                    select: {
+                        defaultVariants: {
+                            size: 'xl',
+                        },
+                    },
+                },
+                colorMode: false,
+            }),
+        ],
+        server: {
+            host: '0.0.0.0',
+            cors: true,
+            hmr: {
+                host: env.VITE_HMR_HOST || 'localhost',
             },
-        }),
-        ui({
-            inertia: true,
-            ui: {
-                colors: {
-                    primary: 'pink',
-                    secondary: 'sky',
-                    neutral: 'slate',
-                },
-
-                button: {
-                    base: 'cursor-pointer',
-                },
-
-                switch: {
-                    defaultVariants: {
-                        size: 'xl',
-                    },
-                },
-
-                input: {
-                    defaultVariants: {
-                        size: 'xl',
-                    },
-                },
-
-                select: {
-                    defaultVariants: {
-                        size: 'xl',
-                    },
-                },
-            },
-            colorMode: false,
-        }),
-    ],
+        },
+    };
 });

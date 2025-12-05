35c2525c946da2c41af0cc9e5232875ad4fd1b4a
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-12-05 20:56:10 +0100

    CAPY-47 add event action buttons

--- resources/js/pages/events/EventShow.vue
+++ resources/js/pages/events/EventShow.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import EventController from '@/actions/App/Http/Controllers/EventController';
 import EventCard from '@/components/events/EventCard.vue';
 import AuthenticatedLayout from '@/layouts/app/AuthenticatedLayout.vue';
 import type { Event } from '@/types/Event';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 const props = defineProps<{
     event: Event;
@@ -20,6 +21,29 @@ const props = defineProps<{
             >
                 <h4 class="text-[10px] font-bold">Poznámka</h4>
                 {{ props.event.description }}
+            </UCard>
+            <UCard :class="props.event.capybara.classes">
+                <div class="flex flex-row items-center justify-around">
+                    <Link
+                        :href="
+                            EventController.create({
+                                query: {
+                                    duplicate_event_id: props.event.id,
+                                },
+                            })
+                        "
+                    >
+                        <UIcon name="i-lucide-copy-plus" class="size-6" />
+                    </Link>
+                    <Link disabled>
+                        <!-- wip: name="i-lucide-square-pen" -->
+                        <UIcon name="i-lucide-construction" class="size-6" />
+                    </Link>
+                    <Link disabled>
+                        <!-- wip: name="i-lucide-square-x" -->
+                        <UIcon name="i-lucide-construction" class="size-6" />
+                    </Link>
+                </div>
             </UCard>
         </div>
     </AuthenticatedLayout>

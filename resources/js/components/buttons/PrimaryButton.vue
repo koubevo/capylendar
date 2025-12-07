7a82a662e0c510ba44baf3dce52b8e1f908c3195
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-12-07 17:38:50 +0100

    CAPY-49 fix desktop layout issues

--- resources/js/components/buttons/PrimaryButton.vue
+++ resources/js/components/buttons/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <UButton color="primary" block class="justify-center" v-bind="$attrs">
+    <UButton color="primary" class="justify-center" v-bind="$attrs">
         <slot />
     </UButton>
 </template>

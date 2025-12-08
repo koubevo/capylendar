45a32b2cb761eb1ae056c41f795186b559d21107
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-12-08 21:48:27 +0100

    CAPY-52 add info components

---
+++ resources/js/components/ui/InfoParagraph.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+    label: string;
+    value: string;
+}>();
+</script>
+
+<template>
+    <div class="mb-4">
+        <h4 class="text-[10px] font-bold">{{props.label}}</h4>
+        {{ props.value }}
+    </div>
+</template>

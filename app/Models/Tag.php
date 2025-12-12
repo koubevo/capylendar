7c465ba9e035f22328aa69b9fbce8ea15f480877
Vojtech Koubek <vojtech.koubek@seznam.cz>
2025-12-12 17:51:37 +0100

    CAPY-19 add TagFactory.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Database\Factories\TagFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -11,6 +13,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  */
 class Tag extends Model
 {
+    /** @use HasFactory<TagFactory> */
+    use HasFactory;
+
     /**
      * @var list<string>
      */

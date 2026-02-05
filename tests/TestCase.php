339639d59adbbe7fa0f6bdb62d32e56b751b6527
Vojtech Koubek <vojtech.koubek@seznam.cz>
2026-02-05 17:51:45 +0100

    fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Prevent rendering error views in tests to avoid Vite manifest issues
+        $this->withoutVite();
+    }
 }

c4b11c24e477e4f959f0f7c23aeb798f8fb49ed8
Vojtech Koubek <vojtech.koubek@seznam.cz>
2026-02-18 20:51:24 +0100

    feat: add neutral button component

---
+++ resources/js/components/buttons/NeutralButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    loading?: boolean;
+}>();
+</script>
+
+<template>
+    <UButton color="neutral" variant="outline" v-bind="$attrs" :loading="loading">
+        <slot />
+    </UButton>
+</template>

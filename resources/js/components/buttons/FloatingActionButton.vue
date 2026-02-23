049252b966c26b990cd5fff83b0a8ab53ef8f7e4
Vojtech Koubek <vojtech.koubek@seznam.cz>
2026-02-23 16:58:32 +0100

    feat!: bump version

--- resources/js/components/buttons/FloatingActionButton.vue
+++ resources/js/components/buttons/FloatingActionButton.vue
@@ -27,7 +27,7 @@ const addMenuItems = [
             icon="i-lucide-plus"
             size="xl"
             color="primary"
-            class="fixed right-6 bottom-6 z-50 flex h-14 w-14 items-center justify-center rounded-lg shadow-xl md:hidden"
+            class="fixed right-6 bottom-6 z-50 flex h-14 w-14 items-center justify-center rounded-lg shadow-xl md:hidden "
             aria-label="Přidat"
         />
     </UDropdownMenu>

a2a108ef85faecdecdb84661d2b08aef8d6475fa
Vojtech Koubek <vojtech.koubek@seznam.cz>
2026-04-24 00:08:55 +0200

    fix: exclude finished todos from dashboard group visibility check

--- resources/js/components/dashboard/DashboardList.vue
+++ resources/js/components/dashboard/DashboardList.vue
@@ -139,7 +139,7 @@ const isHighlighted = (item: DashboardItem) => {
 };
 
 const hasTodosInGroup = (items: DashboardItem[]) => {
-    return items.some((item) => item.type === 'todo');
+    return items.some((item) => item.type === 'todo' && !(item.data as Todo).is_finished);
 };
 
 const postponingDate = ref<string | null>(null);
